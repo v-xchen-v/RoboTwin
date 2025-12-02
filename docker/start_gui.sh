4a08e2003e682de16da3a8fdd01f9bd15692e280
Xi Chen <407081305@qq.com>
2025-12-02 13:10:40 +0800

    shm-size from default 64MB to 8GB for dataloader

--- docker/start_gui.sh
+++ docker/start_gui.sh
@@ -2,6 +2,7 @@ xhost +
 
 sudo docker run -it --rm \
 --gpus all \
+--shm-size=8g \
 -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,graphics \
 -e DISPLAY=$DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
